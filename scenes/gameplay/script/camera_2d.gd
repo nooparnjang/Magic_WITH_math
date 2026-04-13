e51a0f3202633bf031edef8b0e827a1ffe38bcc3
SUTTHIDA JOIROONG <sutthida.joi04@gmail.com>
2026-04-13 22:28:43 +0700

    before hell

--- scenes/gameplay/script/camera_2d.gd
+++ scenes/gameplay/script/camera_2d.gd
@@ -1,13 +1,37 @@
 extends Node2D
 
-@export var target: Node2D
-@export var follow_speed := 4
+@export var default_target: Node2D
+@export var follow_speed := 4.0
 
-func _ready():
+var current_target: Node2D
+var is_focusing := false
+
+func _ready() -> void:
+	current_target = default_target
 	$Camera2D.make_current()
 
-func _process(delta):
-	if target == null:
+func _process(delta: float) -> void:
+	if current_target == null:
 		return
 	
-	global_position = global_position.lerp(target.global_position, follow_speed * delta)
+	global_position = global_position.lerp(
+		current_target.global_position,
+		follow_speed * delta
+	)
+
+func focus_on(temp_target: Node2D, duration: float = 1.0) -> void:
+	if temp_target == null:
+		return
+	if is_focusing:
+		return
+	
+	_focus_routine(temp_target, duration)
+
+func _focus_routine(temp_target: Node2D, duration: float) -> void:
+	is_focusing = true
+	current_target = temp_target
+	
+	await get_tree().create_timer(duration).timeout
+	
+	current_target = default_target
+	is_focusing = false
