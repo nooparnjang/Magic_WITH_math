ac39f350464cd026bd672f9b45db47ed61083889
natsineesampan <natsineesampan@gmail.com>
2026-06-29 17:52:17 +0700

    blockfadefinal

--- object/script/baseinvisibletimer.gd
+++ object/script/baseinvisibletimer.gd
@@ -1,58 +1,157 @@
 extends StaticBody2D
 
-@export var disappear_time: float = 1.0 # เวลากระพริบก่อนหาย
-@export var respawn_time: float = 3.0 # เวลาก่อนกลับมา
-@export var blink_speed: float = 0.15
-@export var wait_a: float = 2.0 # เวลาที่ผู้เล่นยืนก่อนเตือน
 
-var timer_started := false
+@export var stand_time: float = 3.0 # เวลาที่ต้องยืนก่อนหาย
+@export var blink_duration: float = 1.0 # เวลากระพริบก่อนหาย
+@export var respawn_time: float = 3.0
+@export var blink_speed: float = 0.15
+
+
+var player_on_block := false
+var activated := false
+
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("เหยียบ:", body.name)
 
-	if body.is_in_group("player") and !timer_started:
-		timer_started = true
+	if body.is_in_group("player"):
 
-		# รอให้ผู้เล่นยืน 2 วิ
-		await get_tree().create_timer(wait_a).timeout
-		
-		# กระพริบเตือน 3 วิ
-		await blink_warning()
-		
-		# หาย
-		disappear()
+		print("ผู้เล่นเหยียบ")
 
-		# รอ 3 วิแล้วกลับมา
-		await get_tree().create_timer(respawn_time).timeout
-		
-		respawn()
+		player_on_block = true
 
 
-func blink_warning():
-	var time_passed := 0.0
-	
-	while time_passed < disappear_time:
-		$Sprite2D.visible = false
+		if activated:
+			return
+
+
+		activated = true
+
+		start_sequence()
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+
+	if body.is_in_group("player"):
+
+		print("ผู้เล่นออก")
+
+		player_on_block = false
+
+
+
+func start_sequence() -> void:
+
+
+	# รอให้ผู้เล่นยืนครบ 3 วิ
+	await get_tree().create_timer(stand_time).timeout
+
+
+
+	# ถ้าออกก่อน 3 วิ หยุดทันที
+	if not player_on_block:
+
+		print("ออกก่อนครบเวลา ยกเลิก")
+
+		activated = false
+
+		return
+
+
+
+	# ครบ 3 วิแล้ว ค่อยกระพริบ
+	print("ครบ 3 วิ เริ่มเตือน")
+
+
+	await blink_warning()
+
+
+
+	# ถ้าออกตอนกระพริบ ไม่หาย
+	if not player_on_block:
+
+		print("ออกตอนเตือน ยกเลิก")
+
+		$Sprite2D.show()
+
+		activated = false
+
+		return
+
+
+
+	# หาย
+	disappear()
+
+
+
+	# รอ 3 วิ
+	await get_tree().create_timer(respawn_time).timeout
+
+
+
+	respawn()
+
+
+
+
+func blink_warning() -> void:
+
+	var time := 0.0
+
+
+	while time < blink_duration:
+
+
+		if not player_on_block:
+
+			$Sprite2D.show()
+
+			return
+
+
+
+		$Sprite2D.hide()
+
 		await get_tree().create_timer(blink_speed).timeout
-		
-		$Sprite2D.visible = true
+
+
+		$Sprite2D.show()
+
 		await get_tree().create_timer(blink_speed).timeout
-		
-		time_passed += blink_speed * 2
 
 
-func disappear():
+		time += blink_speed * 2
+
+
+
+
+
+func disappear() -> void:
+
 	print("พื้นหาย")
 
-	$CollisionShape2D.disabled = true
+
+	$CollisionShape2D.set_deferred("disabled", true)
+
+	$Area2D/CollisionShape2D.set_deferred("disabled", true)
+
 	$Sprite2D.hide()
 
 
-func respawn():
+
+
+func respawn() -> void:
+
 	print("พื้นกลับมา")
 
-	$CollisionShape2D.disabled = false
+
+	$CollisionShape2D.set_deferred("disabled", false)
+
+	$Area2D/CollisionShape2D.set_deferred("disabled", false)
+
 	$Sprite2D.show()
 
-	timer_started = false
+
+	activated = false
