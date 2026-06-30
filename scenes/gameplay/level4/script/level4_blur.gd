d06cbcc8fca9944b9062001a1f5cdccb7bfb8c50
nooparnjang <nooparnjang@gmail.com>
2026-06-30 23:16:56 +0700

    level 4 done (no mechanic)
    likely 60% laew jra waiting for game mechanic na

--- scenes/gameplay/level4/script/level4_blur.gd
+++ scenes/gameplay/level4/script/level4_blur.gd
@@ -1,2 +1,1 @@
 extends Panel
-z
--- scenes/gameplay/level4/script/robot_haiwei.gd
+++ scenes/gameplay/level4/script/robot_haiwei.gd
@@ -1,10 +1,49 @@
 extends Area2D
 
-func _on_body_entered(body):
-	if !body.is_in_group("robot_hai"):
+@export var next_scene: String = "res://scenes/gameplay/level4/level_4_realmain.tscn"
+
+var player_in_range := false
+
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+
+func _on_body_entered(body: Node2D) -> void:
+
+	# Robot touches teleport
+	if body.is_in_group("robot_hai"):
+
+		var tween = create_tween()
+
+		tween.tween_property(body, "modulate:a", 0.0, 0.25)
+
+		tween.tween_callback(body.queue_free)
+
 		return
 
-	for robot in get_tree().get_nodes_in_group("robot_hai"):
-		var tween = create_tween()
-		tween.tween_property(robot, "modulate:a", 0.0, 0.3)
-		tween.tween_callback(robot.queue_free)
+
+	# Player enters teleport
+	if body.is_in_group("player"):
+		player_in_range = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
+
+
+func _process(_delta):
+
+	if player_in_range and Input.is_action_just_pressed("ui_talk"):
+		change_scene()
+
+
+func change_scene():
+
+	if next_scene == "":
+		push_warning("next_scene is empty!")
+		return
+
+	get_tree().change_scene_to_file(next_scene)
