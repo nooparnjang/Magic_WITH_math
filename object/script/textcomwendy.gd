9a28d02c45a695164a98f6eaaa765f04145b391e
natsineesampan <natsineesampan@gmail.com>
2026-07-04 21:53:25 +0700

    txcomw

--- object/script/textcomwendy.gd
+++ object/script/textcomwendy.gd
@@ -5,11 +5,6 @@ func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		close_ui()
-
-
 func _on_back_pressed():
 	close_ui()
 
